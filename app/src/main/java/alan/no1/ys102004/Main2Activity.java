4acd83c9629a1bd26431c4fa054fcb8f07178d05
alan <p9002>
2016-11-24 12:40:50 +0800

    Initial commit

---
+++ app/src/main/java/alan/no1/ys102004/Main2Activity.java
@@ -0,0 +1,26 @@
+package alan.no1.ys102004;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void click1(View v)
+    {
+        EditText ed = (EditText) findViewById(R.id.editText);
+        String str = ed.getText().toString();
+        Intent it = new Intent();
+        it.putExtra("mydata", str);
+
+        setResult(RESULT_OK, it);
+        finish();
+    }
+}
